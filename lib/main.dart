f996173d01337db9d0d88cb001b4367c360a12f7
qshh <101165772+qinshah@users.noreply.github.com>
2026-05-16 05:54:45 +0800

    ``` refactor(main): 移除未使用的Rust相关代码
    移除了未使用的rinf包导入、bindings导入以及initializeRust调用，
    清理了项目中不再需要的Rust集成代码。
    ```

--- lib/main.dart
+++ lib/main.dart
@@ -6,13 +6,13 @@ import 'package:flutter_bili/service/auth_s.dart';
 import 'package:flutter_bili/service/media_s.dart';
 import 'package:flutter_bili/service/storage_s.dart';
 import 'package:provider/provider.dart';
-import 'package:rinf/rinf.dart';
+// import 'package:rinf/rinf.dart';
 import 'package:u_service/u_service.dart';
 
-import 'src/bindings/bindings.dart';
+// import 'src/bindings/bindings.dart';
 
 Future<void> main() async {
-  await initializeRust(assignRustSignal);
+  // await initializeRust(assignRustSignal);
   WidgetsFlutterBinding.ensureInitialized();
   await Future.wait([StorageS.init(), USystemS.initWindowManager()]);
   MediaS.initLib();
